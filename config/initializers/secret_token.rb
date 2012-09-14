fb0fd6ec32218fe38801db1c6d4cf5a4694f4240
Blizzerand <your.anon.wiki04@gmail.com>
2012-09-15 02:23:01 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rrtut::Application.config.secret_token = '426e54cd7b2254cabfe8291117dd2633f3f0cf982050280dce74775d7ab4688708df46e2cc44c5c3d9ae80566258429c6984c31d4280d074cc5fb024f868d374'
